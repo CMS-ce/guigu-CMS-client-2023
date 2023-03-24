1c54ec10a25912a454c6948da63d376965d118f6
创圣大天使Evol <mf296662402@126.com>
2023-03-24 19:49:08 +0800

    0 跟随官方教程初始化

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+// in src/App.tsx
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
+import { UserList } from './views/users'
+import { PostCreate, PostEdit, PostList } from './views/posts'
+import PostIcon from '@mui/icons-material/Book'
+import UserIcon from '@mui/icons-material/Group'
+import { Dashboard } from './views/dashboard'
+import { authProvider } from './auth/authProvider'
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
+
+const App = () => (
+    <Admin
+        authProvider={authProvider}
+        dataProvider={dataProvider}
+        dashboard={Dashboard}
+    >
+        <Resource
+            name='users'
+            list={UserList}
+            recordRepresentation='name'
+            icon={UserIcon}
+        />
+        <Resource
+            name='posts'
+            list={PostList}
+            edit={PostEdit}
+            create={PostCreate}
+            icon={PostIcon}
+        />
+    </Admin>
+)
+
+export default App
---
+++ src/admin/index.tsx
@@ -0,0 +1,15 @@
+// in src/admin/index.tsx
+import { Admin, Resource, ListGuesser } from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
+import { authProvider } from '../auth/authProvider'
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
+
+const App = () => (
+    <Admin authProvider={authProvider} dataProvider={dataProvider}>
+        <Resource name='posts' list={ListGuesser} />
+        <Resource name='comments' list={ListGuesser} />
+    </Admin>
+)
+
+export default App
